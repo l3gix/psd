899a4e9fa8cbec6646df28a420bdc5f663dea9b2
Giovanni <latellagiovanni2005@gmail.com>
2025-04-09 19:00:19 +0200

    Esercizio Stack Parentesi

---
+++ esercizi_laboratorio/esStack/item.h
@@ -0,0 +1,7 @@
+typedef void* Item;
+
+Item inputItem();
+Item getItem(char c);
+char getChar(Item);
+void outputItem(Item);
+int cmpItem(Item, Item);
---
+++ esercizi_laboratorio/esStack/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "balance.h"
+
+#define MAX 20
+
+
+int main(void){
+	char exp[MAX];
+	printf("Inserisci l'epressione : ");
+	scanf("%[^\n]",exp);
+
+	if(isBalanced(exp)) printf("E bilanciata\n");
+	else printf("Non è bilancita\n");
+
+	return 0;
+}
